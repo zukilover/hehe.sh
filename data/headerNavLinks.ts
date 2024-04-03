06ebdcb41e16feac128f2bb8872e6fd9037455f6
Heri Setiawan <heri.dehero@gmail.com>
2024-04-04 05:47:54 +0700

    Starting hehe.sh

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,9 @@
+const headerNavLinks = [
+  { href: '/', title: 'Home' },
+  { href: '/bugs', title: 'Bugs' },
+  { href: '/labs', title: 'Labs' },
+  { href: '/projects', title: 'Projects' },
+  { href: '/about', title: 'About' },
+]
+
+export default headerNavLinks
